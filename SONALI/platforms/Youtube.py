25100ad8392b440e815b013facdb2d782a9f503c
Heyynitin <heyyyynitin@gmail.com>
2026-01-02 12:29:50 +0530

    Update Youtube.py

--- SONALI/platforms/Youtube.py
+++ SONALI/platforms/Youtube.py
@@ -5,9 +5,9 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from KanhaMusic.utils.formatters import time_to_seconds
+from SONALI.utils.formatters import time_to_seconds
 import aiohttp
-from KanhaMusic import LOGGER
+from SONALI import LOGGER
 
 try:
     from py_yt import VideosSearch
